0749298ddf8eb9844621e5341d0dcfadc690edd7
Denis <rl.cl@yandex.ru>
2022-06-01 18:51:18 +0300

    function

--- .idea/function.php
+++ .idea/function.php
@@ -3,20 +3,46 @@
 $textStorage = [
     0 => [
         'title' => 'Титул',
-    ],
-    1 => [
         'text' => 'Текст',
-    ],
+        ],
 ];
-//$arr = ['vfk' => 'cxv'];
-//array_push($textStorage, $arr,);
-//print_r($textStorage);
 
-function add($arr, $title, $text) {
-    echo $title, $text;
+// Добавление элемента
+function add(&$arr, $title, $text) {
+    $arr[] = [
+        'title' => $title,
+        'text' => $text,
+    ];
 }
 
-add($textStorage, $textStorage[0]['title'], $textStorage[1]['text']);
-add($textStorage, $textStorage[0]['title'], $textStorage[1]['text']);
+add($textStorage, "Букварь", "Заголовок");
+add($textStorage, "Пропись", "Тема");
+
+
+function remove(&$arr, $number) {
+    if (isset($arr[$number])) {
+        unset($arr[$number]);
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+//print_r(remove($textStorage, '0') . PHP_EOL);
+//print_r(remove($textStorage, '5') . PHP_EOL);
+
+function edit(&$arr1, $title, $text, &$arr, $number) {
+    if (isset($arr[$number])) {
+        $arr1 = [
+            'title' => $title,
+            'text' => $text,
+        ];
+        return array_replace($arr, $arr1);
+        return true;
+    }
+}
+edit($textStorage[1], 'dsa', 'asd',$textStorage, '1');
+
+print_r($textStorage);
+
 
 
