e54b83ebf50d20020d10c721015ed6004c8e21bd
Den <rl.cl@yandex.ru>
2022-06-16 16:45:41 +0300

    Translate

--- Translate.php
+++ Translate.php
@@ -1,15 +1,15 @@
 <?php
 
-class Translate
+class Translator
 {
     private $dictionary = [];
 
-    public function addWord($RussianWord, $translite)
+    public function addWord(string $russianWord, string $translite)
     {
         if (array_key_exists($translite, $this->dictionary)) {
             return;
         }
-        $this->dictionary[$translite] = $RussianWord;
+        $this->dictionary[$translite] = $russianWord;
     }
 
     public function translate($englishWord)
@@ -19,4 +19,16 @@ class Translate
         }
         return false;
     }
+}
+
+$translation = new Translator();
+$translation->addWord('Лес', 'forest');
+$translation->addWord('Работа', 'work');
+
+
+$translationResult = $translation->translate('work');
+if ($translationResult !== false) {
+    echo $translationResult . PHP_EOL;
+} else {
+    echo 'Данного слова нет в словаре!';
 }
