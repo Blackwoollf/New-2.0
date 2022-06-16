186e5e64cac68ec494826b53ffde0cbe55994744
Den <rl.cl@yandex.ru>
2022-06-16 17:23:24 +0300

    telegram

---
+++ Telegraf.php
@@ -0,0 +1,48 @@
+<?php
+
+$textStorage = [
+    0 => [
+        'title' => 'Титул',
+        'text' => 'Текст',
+    ],
+];
+
+// Добавление элемента
+function add(&$textStorage, $title, $text) {
+    $textStorage[] = [
+        'title' => $title,
+        'text' => $text,
+    ];
+}
+
+add($textStorage, "Букварь", "Заголовок");
+add($textStorage, "Пропись", "Тема");
+
+//Удаление элемента
+function remove(&$textStorage, $number) {
+    if (isset($textStorage[$number])) {
+        unset($textStorage[$number]);
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+print_r(remove($textStorage, '0') . PHP_EOL);
+print_r(remove($textStorage, '5') . PHP_EOL);
+
+//Изменен элемента
+function edit(&$arrs, $title, $text, &$textStorage) {
+    if (isset($textStorage)) {
+        $arrs = [
+            'title' => $title,
+            'text' => $text,
+        ];
+        return array_replace($textStorage, $arrs);
+    }
+}
+edit($textStorage[1], 'dsa', 'asd',$textStorage);
+
+print_r($textStorage);
+
+
+
