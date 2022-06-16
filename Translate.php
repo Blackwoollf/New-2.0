030f40813735edc215f081ca24b37df6f8c6d28b
Den <rl.cl@yandex.ru>
2022-06-16 16:17:15 +0300

    Translate

--- Translate.php
+++ Translate.php
@@ -1,1 +1,22 @@
 <?php
+
+class Translate
+{
+    private $dictionary = [];
+
+    public function addWord($RussianWord, $translite)
+    {
+        if (array_key_exists($translite, $this->dictionary)) {
+            return;
+        }
+        $this->dictionary[$translite] = $RussianWord;
+    }
+
+    public function translate($englishWord)
+    {
+        if (array_key_exists($englishWord, $this->dictionary)) {
+            return $this->dictionary[$englishWord] . PHP_EOL;
+        }
+        return false;
+    }
+}
