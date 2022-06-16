a4e5d2bcf4713bda0b2d00afc6e4d48b8bdda111
Den <rl.cl@yandex.ru>
2022-06-16 10:32:57 +0300

    Calculate

---
+++ Calculator.php
@@ -0,0 +1,33 @@
+<?php
+
+$supportedOperators = ['+', '-', '*'];
+function calculateOperation (int $a, int $b, string $operator = '+') {
+    if ($operator == '+') {
+        return $a + $b;
+    } elseif ($operator == '-') {
+        return $a - $b;
+    } elseif ($operator == '*') {
+        return $a * $b;
+    }
+}
+
+function parseOperator( $userInput, $operator) {
+    $parseResult = explode($operator, $userInput);
+    if ($parseResult && count($parseResult)== 2) {
+        return ['operators' => $parseResult, 'operator' => $operator];
+    }
+    return false;
+}
+
+stream_set_blocking(STDIN, false);
+
+do {
+    $userInput = readline("Введите выражение ");//7+2
+    foreach ($supportedOperators as $operator) {
+        $parseResult = parseOperator($userInput, $operator);
+        if($parseResult) {
+            echo calculateOperation(intval($parseResult['operators'][0]), intval($parseResult['operators'][1]), $parseResult['operator']);
+        }
+    }
+    $key = ord(fgetc(STDIN));
+} while ($key != 10);
