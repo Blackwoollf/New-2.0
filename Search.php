054b30b39d0ed9712eee77f04e205953f28ba7d8
Den <rl.cl@yandex.ru>
2022-06-16 10:45:15 +0300

    Last Calc

--- Search.php
+++ Search.php
@@ -7,15 +7,19 @@ function search_file($searchRoot, $fileName, &$searchResult)
     $dir = opendir($searchRoot); // открываем текущую папку
 
     // перебираем папку, пока есть файлы
-    while (($file = readdir($dir)) !== false) {
+    while ($file = readdir($dir)) {
         $file_path = "$searchRoot/$file";
         // если это не папка
-        if ($file == '.' || $file == '..') continue;
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
 
         // если это файл, то проверяем имя
         if (is_file($file_path)) {
             // если имя файла искомое, то вернем путь до него и запишем в массив
-                if (false !== strpos($file, $fileName)) $searchResult[] = $file_path ;//. " Размер файл: " . filesize($file_path) . " мб ";
+            if (false !== strpos($file, $fileName)) {
+                $searchResult[] = $file_path;//. " Размер файл: " . filesize($file_path) . " мб ";
+            }
         } // если это папка, то рекурсивно вызываем search_file
         elseif (is_dir($file_path)) {
             $res = search_file($file_path, $fileName, $searchResult);
@@ -35,7 +39,8 @@ $searchResult = array();
 
 $searchResult = search_file($searchRoot, $searchName, $searchResult);
 
-function find ($searchResult) {
+function find($searchResult)
+{
     foreach ($searchResult as $asd) {
         if (filesize($asd) > 0) {
             print_r($asd . " Размер файл: " . filesize($asd) . " мб ");
