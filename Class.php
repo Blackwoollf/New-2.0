b40064feb3892b9ebaa4a4e9083cb7bd873a94b9
Den <rl.cl@yandex.ru>
2022-06-16 12:04:23 +0300

    Class

---
+++ Class.php
@@ -0,0 +1,25 @@
+<?php
+    $employee_1 = ['age' => 30, 'gender' => 'male', 'name' => 'Nick', 'surname' => 'Ivanov', 'position' => 'SEO'];
+    $employee_2 = ['age' => 33, 'gender' => 'female', 'name' => 'Ann', 'surname' => 'Petrova', 'position' => 'STO'];
+
+    function displayName($employee){
+        echo $employee['name']. PHP_EOL;
+    }
+
+    function displayAge($employee){
+        echo $employee['age']. PHP_EOL;
+    }
+
+    function changePosition(&$employee, $newPosition){
+        $employee['position'] = $newPosition;
+    }
+
+    displayName($employee_1);
+    displayName($employee_2);
+
+    displayAge($employee_1);
+    displayAge($employee_2);
+
+    changePosition($employee_1, 'HR');
+
+    print_r($employee_1);
